d8c62c73abfdc8ca4b5f356990d4199c328c1a4a
Nicolas Garnero <nicogarnero1@outlook.com>
2023-02-23 15:31:48 -0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace Lab1PlaceGroup
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            try
+            {
+                //Define a reference Object to accept the pick result
+                Reference pickedRef = null;
+
+                //Pick a group
+                Selection sel = uiapp.ActiveUIDocument.Selection;
+                GroupPickFilter selFilter = new GroupPickFilter();
+                
+                pickedRef = sel.PickObject(ObjectType.Element, selFilter, "Please select a group");
+                Element elem = doc.GetElement(pickedRef);
+                Group group = elem as Group;
+
+                //Pick point
+                XYZ point = sel.PickPoint("Please pick a point to place group");
+
+                //Place the group
+                Transaction trans = new Transaction(doc);
+                trans.Start("Lab");
+                doc.Create.PlaceGroup(point, group.GroupType);
+                trans.Commit();
+                return Result.Succeeded;
+            }
+            //If the user right-clicks or presses Esc, handle the exception
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+        }
+        // Filter to constrain picking to model groups. Only model groups
+        // are highlighted and can be selected when cursor is hovering.
+        public class GroupPickFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element e)
+            {
+                return e.Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_IOSModelGroups);
+            }
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return false;
+            }
+            public bool hola(bool v) { return v; }        
+        }
+
+    }
+
+}
